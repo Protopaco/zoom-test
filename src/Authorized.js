b01055bbc54d03b57dbdcf91d56b2e1d6e8fb695
Paul Stevens <twinkisan@hotmail.com>
2020-11-06 13:27:39 -0800

    adds /auth/ placeholder

---
+++ src/Authorized.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Authorized extends Component {
+    render() {
+        return (
+            <div>
+                'It worked!'
+            </div>
+        )
+    }
+}
