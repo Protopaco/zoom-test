aea32f151ea4bdee58286e7df5d6b0a7c35dfee9
Paul Stevens <twinkisan@hotmail.com>
2020-11-06 17:16:09 -0800

    adds authorization_code step

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import fetch from 'superagent';
 
 
 export default class LandingPage extends Component {
@@ -9,7 +10,11 @@ export default class LandingPage extends Component {
     componentDidMount = async () => {
         let code = new URLSearchParams(window.location.search);
         // this.setState({ code: code.get('code') })
-        this.props.handleSetState({ code: code.get('code') });
+        // `https://zoom.us/oauth/token?grant_type=authorization_code&code=${code.get('code)}&redirect_uri=https://determined-davinci-266c06.netlify.app/`
+
+        let authorizationCode = await fetch.post(`https://zoom.us/oauth/token?grant_type=authorization_code&code=${code.get('code')}&redirect_uri=https://determined-davinci-266c06.netlify.app/`)
+        // this.props.handleSetState({ code: code.get('code') });
+        alert(authorizationCode.body.access_token)
         this.props.history.push('/auth/');
 
     }
