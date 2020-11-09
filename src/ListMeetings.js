3d81bdd650e472d9da3702393515d764629db35b
Paul Stevens <twinkisan@hotmail.com>
2020-11-08 17:50:51 -0800

    displayes username after successful login

--- src/ListMeetings.js
+++ src/ListMeetings.js
@@ -2,30 +2,38 @@ import React, { Component } from 'react'
 import fetch from 'superagent';
 
 export default class ListMeetings extends Component {
+    state = {
+        loading: true
+    }
 
     componentDidMount = async () => {
         // const URL = `https://api.zoom.us/v2/users/{this.props.baseState.code}/recordings`;
-        const URL = 'https://morning-dusk-56874.herokuapp.com/meetings';
+        const URL = 'https://morning-dusk-56874.herokuapp.com/user/';
         try {
-            const returnObject = await fetch.post(URL).send(this.props.baseState.code);
-            console.log(returnObject);
-
+            const returnedObject = await fetch.post(URL).send(this.props.baseState.code);
+            console.log(returnedObject);
+            this.setState({
+                loading: false
+            })
         }
         catch (e) {
             throw e;
         }
-
-        console.log('------------------------------------');
-        console.log(`hi:`)
-        console.log('------------------------------------');
     }
 
     render() {
         return (
             <div>
-                List Meetings
-                <br />
-                {this.props.baseState.code}
+                {this.state.loading
+                    ? <></>
+                    :
+                    <div>
+                        <p>{
+                            `${this.returnedObject.first_name} ${this.returnedObject.last_name}`
+                        }</p>
+                        <img src={this.returnedObject.pic_url} alt="its me!" />
+                    </div>
+                }
             </div>
         )
     }
