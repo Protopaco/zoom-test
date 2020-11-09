da5d485635c2eccb43feb3a1578bfbe6163b5049
Paul Stevens <twinkisan@hotmail.com>
2020-11-08 17:57:04 -0800

    error checking

--- src/ListMeetings.js
+++ src/ListMeetings.js
@@ -9,32 +9,32 @@ export default class ListMeetings extends Component {
     componentDidMount = async () => {
         // const URL = `https://api.zoom.us/v2/users/{this.props.baseState.code}/recordings`;
         const URL = 'https://morning-dusk-56874.herokuapp.com/user/';
-        try {
-            const returnedObject = await fetch.post(URL).send(this.props.baseState.code);
-            console.log(returnedObject);
-            this.setState({
-                loading: false
-            })
-        }
-        catch (e) {
-            throw e;
+        //     try {
+        //         const returnedObject = await fetch.post(URL).send(this.props.baseState.code);
+        //         console.log(returnedObject);
+        //         this.setState({
+        //             loading: false
+        //         })
+        //     }
+        //     catch (e) {
+        //         throw e;
+        //     }
+        // }
+        console.log(this.props.baseState.code);
+        render() {
+            return (
+                <div>
+                    {this.state.loading
+                        ? <></>
+                        :
+                        <div>
+                            <p>{
+                                `${this.returnedObject.first_name} ${this.returnedObject.last_name}`
+                            }</p>
+                            <img src={this.returnedObject.pic_url} alt="its me!" />
+                        </div>
+                    }
+                </div>
+            )
         }
     }
-
-    render() {
-        return (
-            <div>
-                {this.state.loading
-                    ? <></>
-                    :
-                    <div>
-                        <p>{
-                            `${this.returnedObject.first_name} ${this.returnedObject.last_name}`
-                        }</p>
-                        <img src={this.returnedObject.pic_url} alt="its me!" />
-                    </div>
-                }
-            </div>
-        )
-    }
-}
